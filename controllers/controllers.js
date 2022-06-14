79a1b3f33560d0c66803bcbc466fc5d577f57b44
aserber <alan.serber01@gmail.com>
2022-06-14 20:51:10 -0300

    le agregue nodemon

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -5,41 +5,43 @@ const controllers = {
     menu: (req, res) => {
         return res.render('menu.ejs');
     },
-list: (req, res)=> {
-    let menu = [
-        {
-            id: 1,
-            nombre: "Carpaccio fresco",
-            precio: "U$S 65.5",
-            imagen: /images/menu.jpg,
-            detalle: "Entrada Carpaccio de salmón con cítricos"
-        },
-        {
-            id: 2,
-            nombre: "Risotto de berenjena",
-            precio: "U$S 47.00",
-            imagen: /images/Risotto-berenjena-queso-cabra.jpg,
-            detalle: "Risotto de berenjena y queso de cabra"
-        },
-        {
-            id: 3,
-            nombre: "Mousse de arroz",
-            precio: "U$S 27.50",
-            imagen: /images/Mousse-de-arroz-con-leche.jpg,
-            detalle: "Mousse de arroz con leche y aroma de azahar"  
-        }, 
-    
-        {
-            id: 4,
-            nombre: "Espárragos blancos",
-            precio: "U$S 37.50",
-            imagen: /images/esparragos.png,
-            detalle: "Espárragos blancos con vinagreta de verduras y jamón ibérico"  
-        },
-        ]
 
-    return res.render('detalleMenu', {menu: menu});
-},
+
+    list: (req, res)=> {
+        let menu = [
+            {
+                id: 1,
+                nombre: "Carpaccio fresco", 
+                precio: "U$S 65.5",
+                imagen: /images/menu.jpg,
+                detalle: "Entrada Carpaccio de salmón con cítricos"
+            },
+            {
+                id: 2,
+                nombre: "Risotto de berenjena",
+                precio: "U$S 47.00",
+                imagen: /images/Risotto-berenjena-queso-cabra.jpg,
+                detalle: "Risotto de berenjena y queso de cabra"
+            },
+            {
+                id: 3,
+                nombre: "Mousse de arroz",
+                precio: "U$S 27.50",
+                imagen: /images/Mousse-de-arroz-con-leche.jpg,
+                detalle: "Mousse de arroz con leche y aroma de azahar"  
+            }, 
+        
+            {
+                id: 4,
+                nombre: "Espárragos blancos",
+                precio: "U$S 37.50",
+                imagen: /images/esparragos.png,
+                detalle: "Espárragos blancos con vinagreta de verduras y jamón ibérico"  
+            },
+            ]
+    
+        return res.render('detalleMenu', {menu: menu});
+    },
 
 };
 
